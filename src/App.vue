86d6ac5e378d44ed640a62583ca3f6fedbe10873
RaduAntonie86 <128983932+RaduAntonie86@users.noreply.github.com>
2024-08-26 19:36:07 +0300

    Removed unnecessary files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="text-9xl">aaaaaaaaaaaaaaaaaaa</div>
+  <div class="text-9xl">aaaaaaaaaaaaasaaaaaa</div>
   <font-awesome-icon icon="phone" />
 </template>
