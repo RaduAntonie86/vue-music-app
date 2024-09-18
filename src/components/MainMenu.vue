778797d87e19d88d4c339d41510e4d4e82517711
RaduAntonie86 <128983932+RaduAntonie86@users.noreply.github.com>
2024-09-18 18:11:42 +0300

    Added a basic backend component

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -7,10 +7,10 @@
         <left-sidebar class="row-span-10 min-h-[75vh]"></left-sidebar>
     </div>
     <div class="col-span-6">
-        <!-- <main-page class="min-h-[84.8vh]"></main-page> -->
+        <main-page class="min-h-[84.8vh]"></main-page>
         <!-- <search-page class="min-h-[84.8vh]"></search-page> -->
         <!-- <playlist-page class="min-h-[84.8vh]"></playlist-page> -->
-        <album-page class="min-h-[84.8vh]"></album-page>
+        <!-- <album-page class="min-h-[84.8vh]"></album-page> -->
     </div>
 </div>
 </template>
